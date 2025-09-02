c1ea71c59f622a7a6d8fa2ef8ae1846f688bc6ce
ankit-pandit07 <panditankit865@gmail.com>
2025-09-02 13:45:30 +0530

    your message

---
+++ common/dist/index.d.ts
@@ -0,0 +1,27 @@
+import z from "zod";
+declare const signupInput: z.ZodObject<{
+    email: z.ZodString;
+    password: z.ZodString;
+    name: z.ZodOptional<z.ZodString>;
+}, "strip", z.ZodTypeAny, {
+    email: string;
+    password: string;
+    name?: string | undefined;
+}, {
+    email: string;
+    password: string;
+    name?: string | undefined;
+}>;
+export type signupInput = z.infer<typeof signupInput>;
+export declare const signinInput: z.ZodObject<{
+    username: z.ZodString;
+    password: z.ZodString;
+}, "strip", z.ZodTypeAny, {
+    password: string;
+    username: string;
+}, {
+    password: string;
+    username: string;
+}>;
+export type signinInput = z.infer<typeof signinInput>;
+export {};
